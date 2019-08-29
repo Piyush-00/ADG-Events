8992458defde988ba6ad2f919f2c16f92ebf1e2a
pateldevang <devangpatel92@gmail.com>
2019-08-29 21:18:20 +0530

    profile scroll View

--- Events/Controllers/profileViewController.swift
+++ Events/Controllers/profileViewController.swift
@@ -10,21 +10,42 @@ import UIKit
 
 class profileViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+//extension UIView {
+//
+//    // OUTPUT 1
+//    func dropShadow(scale: Bool = true) {
+//        layer.masksToBounds = false
+//        layer.shadowColor = UIColor.black.cgColor
+//        layer.shadowOpacity = 0.5
+//        layer.shadowOffset = CGSize(width: -0.5, height: 0.5)
+//        layer.shadowRadius = 0.5
+//
+//        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+//        layer.shouldRasterize = true
+//        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+//    }
+//
+//    // OUTPUT 2
+//    func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+//        layer.masksToBounds = false
+//        layer.shadowColor = color.cgColor
+//        layer.shadowOpacity = opacity
+//        layer.shadowOffset = offSet
+//        layer.shadowRadius = radius
+//
+//        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+//        layer.shouldRasterize = true
+//        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+//    }
+//}
